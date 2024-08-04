2ef50c7722d56b4ea1bbf6f77f2c007fda803abf
luiz.escobal <luiz.escobal@profiz.com.br>
2024-08-04 06:10:11 -0400

    ABM-1: creates a valid AccountBank instance using the factory

--- tests/Unit/AccountBankFactoryTest.php
+++ tests/Unit/AccountBankFactoryTest.php
@@ -9,6 +9,6 @@ it('creates a valid AccountBank instance using the factory', function () {
     expect($accountBank->id)->toBeString()->not()->toBeEmpty();
     expect($accountBank->name)->toBeString()->not()->toBeEmpty();
     expect($accountBank->balance)->toBeString()->not()->toBeEmpty();
-    //uuid validation
-    expect($accountBank->id)->strlen(36);
+    // uuid validation length
+    expect($accountBank->id)->toHaveLength(36);
 });
